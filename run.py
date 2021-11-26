f9a867b79e09ce27de6d0cc70e88fb1404d8e946
rodrigodadam <rodrigodadam@gmail.com>
2021-11-26 09:00:58 +0000

    Create get_weather func

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
 import requests
 import os
 from datetime import datetime
-from creds import API_KEY
 
-api_key = os.environ['API_KEY']
-location = input("Enter the city name:")
+API_KEY = os.environ.get('API_KEY')
+
+
+def get_weather():
+    """
+    Get the today's weather forecast for determined city
+    """
+    print("Please Enter the City Name.")
+    print("City should be a real city.")
+    print("Exemple: Dublin")
+
+    city = input("Enter the city name here: ")
+    print(f"This is your provided city: {city}")
+
+
+get_weather()
