0cb833f7e95a98893b5ebfe644453be3099b6ba2
rodrigodadam <rodrigodadam@gmail.com>
2021-11-26 15:18:39 +0000

    PEP8 corrections

--- run.py
+++ run.py
@@ -18,34 +18,34 @@ def get_weather():
 
         city = input("Enter the city name here: ")
         # print(f"This is your provided city: {city}")
-        api_url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}".format(city, KEY)
+        api_url = (
+            'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
+            .format(city, KEY))
         result = requests.get(api_url)
         data = result.json()
 
         temp = ((data['main']['temp']) - 273.15)
         feels_like = ((data['main']['feels_like']) - 273.15)
+        temp_min = ((data['main']['temp_min']) - 273.15)
+        temp_max = ((data['main']['temp_max']) - 273.15)
         weather_description = data['weather'][0]['description']
         air_humidity = data['main']['humidity']
         wind_speed = data['wind']['speed']
         date_time = datetime.now().strftime("%d %b %Y")
-        
+
         if validate_city(data, city):
             break
-    
-        
 
-
-    print("***************************************************")
-    print(f"Weather Status for {city} - Today {date_time}")
-    print("***************************************************")
-
-    print(f"Current Temperature is ------> {temp:.2f} degrees Celsius")
-    print(f"Feels Like ------------------> {feels_like:.2f} degrees Celsius")
-    print(f"Current Weather Description -> {weather_description}")
-    print(f"Current Humidity ------------> {air_humidity}%")
-    print(f"Current Wind Speed-----------> {wind_speed} kmph")
-
-    
+    print("*****************************************************")
+    print(f"*** Weather Status for {city} - Today {date_time} ***")
+    print("*****************************************************")
+    print(f"Current Temperature is -------> {temp:.2f} degrees Celsius")
+    print(f"Feels Like -------------------> {feels_like:.2f} degrees Celsius")
+    print(f"Max Temperature for Today is -> {temp_max:.2f} degrees Celsius")
+    print(f"Min Temperature for Today is -> {temp_min:.2f} degrees Celsius")
+    print(f"Current Weather Description --> {weather_description.title()}")
+    print(f"Current Humidity -------------> {air_humidity}%")
+    print(f"Current Wind Speed------------> {wind_speed} kmph")
 
 
 def validate_city(data, city):
@@ -59,9 +59,6 @@ def validate_city(data, city):
         return False
     else:
         return True
-
-
-
 
 
 get_weather()
